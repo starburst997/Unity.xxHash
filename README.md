caa468ef6770e2a6bc551ed53b9cc76b6c4a4b9f
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-06-09 13:48:10 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Unity.xxHash
-xxHash for Unity UPM
+
+Extremely fast non-cryptographic hash algorithm [xxhash](http://www.xxhash.com/)
+
+Simple UPM package for Unity for my personal use (fork).
+
+## Installation
+
+Add the dependency to your `manifest.json`
+
+```json
+{
+  "dependencies": {
+    "jd.boiv.in.xxhash": "https://github.com/starburst997/Unity.xxHash.git"
+  }
+}
+```
+
+## Usage
+
+Simply call the following to get an id:
+
+```csharp
+var hash = xxHash64.ComputeHash(data, data.Length);
+Debug.Log($"Hash: {hash}");
+```
+
+## TODO
+
+- Better readme
+
+## Credits
+
+Based on [xxHash.st](https://github.com/uranium62/xxHash) which is based on the og [xxHash](https://github.com/Cyan4973/xxHash).
---
+++ Runtime/xxHash64.Span.cs
@@ -0,0 +1,49 @@
+﻿#if !NETSTANDARD2_0 && !NET48
+namespace Standart.Hash.xxHash
+{
+    using System;
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+
+    public static partial class xxHash64
+    {
+        /// <summary>
+        /// Compute xxHash for the data byte span
+        /// </summary>
+        /// <param name="data">The source of data</param>
+        /// <param name="length">The length of the data for hashing</param>
+        /// <param name="seed">The seed number</param>
+        /// <returns>hash</returns>
+        public static unsafe ulong ComputeHash(Span<byte> data, int length, ulong seed = 0)
+        {
+            Debug.Assert(data != null);
+            Debug.Assert(length >= 0);
+            Debug.Assert(length <= data.Length);
+
+            fixed (byte* pData = &MemoryMarshal.GetReference(data))
+            {
+                return ComputeHash(pData, length, seed);
+            }
+        }
+        
+        /// <summary>
+        /// Compute xxHash for the data byte span
+        /// </summary>
+        /// <param name="data">The source of data</param>
+        /// <param name="length">The length of the data for hashing</param>
+        /// <param name="seed">The seed number</param>
+        /// <returns>hash</returns>
+        public static unsafe ulong ComputeHash(ReadOnlySpan<byte> data, int length, ulong seed = 0)
+        {
+            Debug.Assert(data != null);
+            Debug.Assert(length >= 0);
+            Debug.Assert(length <= data.Length);
+
+            fixed (byte* pData = &MemoryMarshal.GetReference(data))
+            {
+                return ComputeHash(pData, length, seed);
+            }
+        }
+    }
+}
+#endif
